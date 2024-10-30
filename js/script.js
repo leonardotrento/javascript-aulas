b3ab579e248aab7ab07dc673823e97e8579682e3
Leonardo Trento <leotrento88@gmail.com>
2024-10-30 15:11:36 -0300

    aulas

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function clique(){
+    //alert('clicou')
+    document.body.style.backgroundColor = "yellow"
+}
+
+function clique2(){
+    alert('evento de clique duplo')
+}
+
+function viravermelho(){
+    document.getElementById('teste')
+    teste.style.backgroundColor = "red"
+}
+
+function viraazul(){
+    document.getElementById('teste')
+    teste.style.backgroundColor = "blue"
+}
